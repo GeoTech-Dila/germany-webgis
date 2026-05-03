09c319ee11646f3892b7bc1da5bdd397554b8e4a
GeoTech-Dila <ozturkdila670@gmail.com>
2026-05-03 14:04:37 +0300

    add cors

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import json
 
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # herkes erişebilir
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 def home():
     return {"status": "API çalışıyor"}
